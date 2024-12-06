edb1f95412d42ad8a033e11250737616d7351367
Jamie Wilson <howemanning@gmail.com>
2024-12-07 05:01:03 +1300

    Start project

--- components/ExternalLink.tsx
+++
@@ -1,8 +0,0 @@
-import { Link } from "expo-router";
-import { type ComponentProps } from "react";
-
-type Props = ComponentProps<typeof Link>;
-
-export function ExternalLink({ href, ...rest }: Props) {
-  return <Link href={href} target="_blank" {...rest} />;
-}
